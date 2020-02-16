96d866dae0b8e05a16cc0fb1f778abad7e941594
rleblanc395 <59615255+rleblanc395@users.noreply.github.com>
2020-02-16 13:19:48 -0700

    done

--- ICA4/ICA4/Form1.cs
+++ ICA4/ICA4/Form1.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//****************************************************************************************
+
+
+
+
+//*****************************************************************************************
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,46 +18,55 @@ namespace ICA4
 {
     public partial class Form1 : Form
     {
-        private List<byte> _KeyCodes = new List<byte>();
-        private int _onesCount = 0;
-        private int _longRun = 0;
+        private List<byte> _KeyCodes = new List<byte>();    //key codes in binary list
+        private int _onesCount = 0;                         // total number of ones
         public Form1()
         {
             InitializeComponent();
         }
 
+        //
         private void CodeBox_KeyDown(object sender, KeyEventArgs e)
         {
+            _onesCount = 0;
             // write kew pressed on label
             KCLbl.Text = e.KeyData.ToString();
             // put key code into the List Box in 2-byte binary
             CodeBox.Items.Add(Convert.ToString(e.KeyValue, 2).PadLeft(8, '0'));
             // add the value to the list
             _KeyCodes.Add((byte)e.KeyValue);
-            // Adjust ones count
-            CountOnes(_KeyCodes[CodeBox.SelectedIndex], ref _onesCount);
-            OCLbl.Text = _onesCount.ToString();
+            // Count total 1's and adjust label
+            foreach (byte key in _KeyCodes)
+            {
+                CountOnes(key, ref _onesCount);
+                OCLbl.Text = _onesCount.ToString();
+            }
         }
 
         private void CodeBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            // Find Longest Run and adjust longest run label
             LRLbl.Text = CountRuns(_KeyCodes[CodeBox.SelectedIndex]).ToString();
         }
         
 
         private int CountRuns(byte currentCode)
         {
-            int longestRun = 0;
-            int check = 1;
-            int nowLong = 0;
+            int longestRun = 0;     // length of longest run
+            int check = 1;          // used for bitwise ops to check if set
+            int nowLong = 0;        // stores length of current run being checked
+            // runs code until every bit is checked
             while ( check < currentCode)
-	{
+	        {
+                // check if a bit is a 1
                 if ((check & currentCode) != 0)
                 {
+                    // count number of 1's in a row
                     nowLong++;
+                    // checks for a zero after a one to end the run 
                     if ((((check << 1) & currentCode)) == 0)
                     {
+                        // record longest run
                         if (nowLong > longestRun)
                             longestRun = nowLong;
                         nowLong = 0;
@@ -63,9 +78,11 @@ namespace ICA4
         }
         private void CountOnes(byte currentCode, ref int onesCount)
         {
-            int check = 1;
+            int check = 1;      // used to check if a bit is set
+            // run through every bit
             while ( check < currentCode)
-	{
+	        {
+                // count number of ones
                 if ((check & currentCode) != 0)
                     onesCount++;
                 check = check << 1;
