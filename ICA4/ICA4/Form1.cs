10864b58ea6e5d4405b40cd2cae7b2beba83ff33
rleblanc395 <59615255+rleblanc395@users.noreply.github.com>
2020-02-16 13:51:16 -0700

    comment

--- ICA4/ICA4/Form1.cs
+++ ICA4/ICA4/Form1.cs
@@ -1,8 +1,8 @@
 ﻿//****************************************************************************************
-
-
-
-
+// ICA04
+// Ryan LeBlanc
+// CNTAO3
+// Kevin Moore
 //*****************************************************************************************
 using System;
 using System.Collections.Generic;
@@ -18,38 +18,44 @@ namespace ICA4
 {
     public partial class Form1 : Form
     {
-        private List<byte> _KeyCodes = new List<byte>();    //key codes in binary list
-        private int _onesCount = 0;                         // total number of ones
+        private List<byte> _keyCodes = new List<byte>();    //key codes in binary list                       
         public Form1()
         {
             InitializeComponent();
         }
 
-        //
+        // A key is pressed
+        // A byte is added to the _keyCodes list
         private void CodeBox_KeyDown(object sender, KeyEventArgs e)
         {
-            _onesCount = 0;
+            int onesCount = 0;      // total number of ones
             // write kew pressed on label
             KCLbl.Text = e.KeyData.ToString();
             // put key code into the List Box in 2-byte binary
             CodeBox.Items.Add(Convert.ToString(e.KeyValue, 2).PadLeft(8, '0'));
             // add the value to the list
-            _KeyCodes.Add((byte)e.KeyValue);
+            _keyCodes.Add((byte)e.KeyValue);
             // Count total 1's and adjust label
-            foreach (byte key in _KeyCodes)
+            foreach (byte key in _keyCodes)
             {
-                CountOnes(key, ref _onesCount);
-                OCLbl.Text = _onesCount.ToString();
+                CountOnes(key, ref onesCount);
+                OCLbl.Text = onesCount.ToString();
             }
         }
 
+        // a key code is selected from the listbox
         private void CodeBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             // Find Longest Run and adjust longest run label
-            LRLbl.Text = CountRuns(_KeyCodes[CodeBox.SelectedIndex]).ToString();
+            LRLbl.Text = CountRuns(_keyCodes[CodeBox.SelectedIndex]).ToString();
         }
-        
 
+        //************************************************************************************************
+        // Method:      private int CountRuns(byte currentCode)
+        // Purpose:     Find the longest run in a selected byte 
+        // Parameters:  byte currentCode    - the byte to examine
+        // Returns:     int longestRun      - the length of the longest run in the entered byte
+        //************************************************************************************************
         private int CountRuns(byte currentCode)
         {
             int longestRun = 0;     // length of longest run
@@ -76,6 +82,13 @@ namespace ICA4
             }
             return longestRun;
         }
+        //************************************************************************************************
+        // Method:      private void CountOnes(byte currentCode, ref int onesCount)
+        // Purpose:     Count the total number of 1's in a byte list
+        // Parameters:  byte currentCode    - the byte to examine
+        //              ref int onesCount   - the number 1's in the list of byte
+        // Returns:     int onesCount       - the total number of ones in the byte list
+        //************************************************************************************************
         private void CountOnes(byte currentCode, ref int onesCount)
         {
             int check = 1;      // used to check if a bit is set
